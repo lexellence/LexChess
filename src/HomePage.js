e11974dd7073a0046802e90a667ede0595ccc910
David Leksen <dleksen@protonmail.com>
2021-11-02 00:42:03 -0400

    Add links to home page

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import * as ROUTES from "./constants/routes";
 import { AuthUserContext } from './Session';
 
 const HomePageNonAuth = () => (
 	<div align='center'>
 		<h1>Welcome to Lex Chess!</h1>
-		<p>Please sign in.</p>
+		<h5>Please <Link to={ROUTES.SIGN_IN}>sign in</Link>.</h5>
 	</div>
 );
 
 const HomePageAuth = ({ authUser }) => (
 	<div align='center'>
 		<h1>Welcome back, {authUser.displayName}!</h1>
+		<h5>Start a <Link to={ROUTES.GAME_LIST}>new game</Link>.</h5>
 	</div>
 );
 
