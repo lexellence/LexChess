174a0dd387bd816797bffef5067142a7023bfe45
David Leksen <dleksen@protonmail.com>
2023-11-30 17:13:47 -0500

    Cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-// import App from './App.tsx'
 import { App } from "./App";
 import { Firebase, FirebaseContext } from './Firebase';
 import { AuthUserProvider } from './Session';
